03c937ff8252175ac77cd73bbdaae29ebf9faaa6
rotem <rotemtzaban@gmail.com>
2018-12-02 01:32:46 +0200

    added downloader on browser side

---
+++ browser/downloadStream.ts
@@ -0,0 +1,48 @@
+export function downloadStream<T = any>(filename: string): WritableStream<T> {
+    const channel = new MessageChannel();
+    navigator.serviceWorker
+        .register('sw.bundle.js', {
+            scope: './'
+        })
+        .then(registration => {
+            const regWaiting = registration.installing || registration.waiting;
+            if (registration.active) {
+                registration.active.postMessage({ filename }, [channel.port2]);
+            }
+
+            if (regWaiting) {
+                regWaiting.onstatechange = ev => {
+                    if (regWaiting.state === 'activated') {
+                        registration.active!.postMessage({ filename }, [
+                            channel.port2
+                        ]);
+                    }
+                };
+            }
+        });
+    return new WritableStream<T>({
+        start() {
+            return new Promise(resolve => {
+                channel.port1.onmessage = ev => {
+                    resolve();
+                    const url: string = ev.data.uniqueUrl;
+                    const link = document.createElement('a');
+                    link.href = url;
+                    link.download = filename;
+
+                    const click = new MouseEvent('click');
+                    link.dispatchEvent(click);
+                };
+            });
+        },
+        write(data) {
+            channel.port1.postMessage(data);
+        },
+        close() {
+            channel.port1.postMessage('end');
+        },
+        abort() {
+            channel.port1.postMessage('abort');
+        }
+    });
+}
--- sw/index.ts
+++ sw/index.ts
@@ -1,4 +1,3 @@
-import { string } from 'prop-types';
 
 const selfRef = self as ServiceWorkerGlobalScope;
 
